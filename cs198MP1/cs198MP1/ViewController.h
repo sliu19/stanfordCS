af61f001db0e29298962af8e87e282d0a2733137
sliu19 <sliu19@illinois.edu>
2015-01-19 17:28:42 -0600

    init

---
+++ cs198MP1/cs198MP1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cs198MP1
+//
+//  Created by Simin on 1/19/15.
+//  Copyright (c) 2015 Simin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
